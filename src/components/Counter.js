61cf18062188cddc837428b9b8bfb15d49854fc8
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-03-31 13:24:36 +0100

    Dispatching actions from inside component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,33 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
+import { useState } from 'react';
 
 const Counter = () => {
+  const [toggle, setToggle] = useState(false)
+  const dispatch = useDispatch()
 
   const counter =useSelector(state => state.counter)
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    setToggle((prev) => !prev)
+  };
+
+  const increamentHandler = () => {
+    dispatch({type: "INCREAMENT"})
+  }
+
+  const decreamentHandler = () => {
+    dispatch({type: "DECREAMENT"})
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {toggle && <div className={classes.value}>{counter}</div>}
+      {toggle && <div>
+        <button onClick={increamentHandler}>INCREAMENT</button>
+        <button onClick={decreamentHandler}>DECREAMENT</button>
+      </div>}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
