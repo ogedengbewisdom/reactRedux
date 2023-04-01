615be5139e4a0683ce8a354b84848e47f9aea154
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-01 10:35:58 +0100

    Re-Evaluating the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Fragment } from 'react';
 import Counter from './components/Counter';
+import Headers from "./components/Header"
+import Auth from "./components/Auth"
+import UserProfile from './components/UserProfile';
+import { useSelector } from 'react-redux';
 
 
 function App() {
+  const show = useSelector(state => state.auth.authentication)
+  
   return (
-    <Counter />
+    <Fragment>
+      <Headers />
+      {show && <UserProfile />}
+      {!show && <Auth />}
+       {show && <Counter />}
+    </Fragment>
   );
 }
 
